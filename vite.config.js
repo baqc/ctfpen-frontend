b62f805cbe56cc16d97b7ac2f738128dd6f6fd80
lixworth <lixworth@gmail.com>
2022-03-16 11:38:30 +0800

    chore: support ie11 by @vitejs/plugin-legacy

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import Components from 'unplugin-vue-components/vite'
 // import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        vue()
+        vue(),
+        legacy({
+            targets: ['ie >= 11'],
+            additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+        })
     ],
     resolve: {
         alias: {
